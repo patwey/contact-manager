0fd39bd7d7a71599917003ea94e08e83951b7f53
Pat Wey <patrickwey@gmail.com>
2015-10-31 13:57:12 -0600

    svelte phone number form

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,34 +1,26 @@
 class PhoneNumbersController < ApplicationController
   before_action :set_phone_number, only: [:show, :edit, :update, :destroy]
 
-  # GET /phone_numbers
-  # GET /phone_numbers.json
   def index
     @phone_numbers = PhoneNumber.all
   end
 
-  # GET /phone_numbers/1
-  # GET /phone_numbers/1.json
   def show
   end
 
-  # GET /phone_numbers/new
   def new
-    @phone_number = PhoneNumber.new
+    @phone_number = PhoneNumber.new(person_id: params[:person_id])
   end
 
-  # GET /phone_numbers/1/edit
   def edit
   end
 
-  # POST /phone_numbers
-  # POST /phone_numbers.json
   def create
     @phone_number = PhoneNumber.new(phone_number_params)
 
     respond_to do |format|
       if @phone_number.save
-        format.html { redirect_to @phone_number, notice: 'Phone number was successfully created.' }
+        format.html { redirect_to @phone_number.person, notice: 'Phone number was successfully created.' }
         format.json { render :show, status: :created, location: @phone_number }
       else
         format.html { render :new }
@@ -37,8 +29,6 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /phone_numbers/1
-  # PATCH/PUT /phone_numbers/1.json
   def update
     respond_to do |format|
       if @phone_number.update(phone_number_params)
@@ -51,8 +41,6 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # DELETE /phone_numbers/1
-  # DELETE /phone_numbers/1.json
   def destroy
     @phone_number.destroy
     respond_to do |format|
@@ -62,12 +50,12 @@ class PhoneNumbersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_phone_number
       @phone_number = PhoneNumber.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def phone_number_params
       params.require(:phone_number).permit(:number, :person_id)
     end
--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -44,6 +44,9 @@ RSpec.describe PhoneNumbersController, type: :controller do
 
   describe "POST #create" do
     context "with valid params" do
+      let(:alice) { Person.create(first_name: 'Alice', last_name: 'Smith') }
+      let(:valid_attributes) { {number: '555-8888', person_id: alice.id} }
+
       it "creates a new PhoneNumber" do
         expect {
           post :create, {:phone_number => valid_attributes}, valid_session
@@ -56,9 +59,9 @@ RSpec.describe PhoneNumbersController, type: :controller do
         expect(assigns(:phone_number)).to be_persisted
       end
 
-      it "redirects to the created phone_number" do
+      it "redirects to the phone_number's person" do
         post :create, {:phone_number => valid_attributes}, valid_session
-        expect(response).to redirect_to(PhoneNumber.last)
+        expect(response).to redirect_to(alice)
       end
     end
 
